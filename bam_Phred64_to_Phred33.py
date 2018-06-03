e29fe84950843faa6d559f52749046e6d1c8d84c
Bracey <39830244+758345@users.noreply.github.com>
2018-06-03 11:45:02 +0800

    Create bam_Phred64_to_Phred33.py

---
+++ bam_Phred64_to_Phred33.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#base quality conversion Phred64 to Phred33 scale for bam file.
+#used as: samtools view -h phred64.bam | path/bam_Phred64_to_Phred33.py | samtools view -Sb - > ./phred33.bam
+import sys
+
+for line in sys.stdin:
+    if line.startswith('@'): #header
+        print line,
+    else:                    #alignments
+        raw  = line.split('\t')
+        base_qual = ''.join([chr(ord(i)-31) for i in raw[10]]) #64 becomes 33
+        if len(raw)>10:
+            print '\t'.join(raw[:10]+[base_qual]+raw[11:]),
+        else:
+            print '\t'.join(raw[:10]+[base_qual]),
